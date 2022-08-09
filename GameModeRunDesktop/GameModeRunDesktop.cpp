c55e3772bc9aa568eb61dd0e6190d4f76e7591dd
Henry Cheng <39224097+jazzysoggy@users.noreply.github.com>
2022-08-09 13:37:07 -0400

    First Working Version

--- GameModeRunDesktop/GameModeRunDesktop.cpp
+++ GameModeRunDesktop/GameModeRunDesktop.cpp
@@ -7,37 +7,43 @@
 
 #include "windows.h"
 #include "stdlib.h"
-
+#include <lowlevelmonitorconfigurationapi.h>
 #include "iostream"
+#include "vector"
 
 #ifdef _DEBUG
 #define new DEBUG_NEW
 #endif
 
 
-// The one and only application object
-
-
 int main()
 {
 
+
+	QUERY_USER_NOTIFICATION_STATE result;
+	QUERY_USER_NOTIFICATION_STATE* ptr = &result;
+	SHQueryUserNotificationState(ptr);
+	QUERY_USER_NOTIFICATION_STATE lastResult = result;
 	// TODO: code your application's behavior here.
 	while (true)
 	{
 		Sleep(1000);
 
-		QUERY_USER_NOTIFICATION_STATE result;
-
 		QUERY_USER_NOTIFICATION_STATE* ptr = &result;
 		SHQueryUserNotificationState(ptr);
-		std::cout << result;
-		if (result == QUNS_RUNNING_D3D_FULL_SCREEN || result == QUNS_BUSY)
+
+		if (result != lastResult)
 		{
-			SendMessage(handle, WM_SYSCOMMAND, SC_MONITORPOWER, 2);
+			if (result == QUNS_RUNNING_D3D_FULL_SCREEN || result == QUNS_BUSY)
+			{
+				system("DisplaySwitch.exe /internal");
+			}
+			else
+			{
+				system("DisplaySwitch.exe /extend");
+			}
 		}
-		else
-		{
-			std::cout << "Not works";
-		}
+
+		lastResult = result;
 	}
 }
